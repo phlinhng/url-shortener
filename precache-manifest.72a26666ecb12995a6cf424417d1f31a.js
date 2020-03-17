8c80163bee11ef297155ad8d17c0baf3b17c0d61
phlinhng <phlinhng@outlook.com>
2020-03-18 03:40:36 +0800

    Updates

---
+++ precache-manifest.72a26666ecb12995a6cf424417d1f31a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5850be0c0c8ac9925f9d2178bc81ae99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce954e224e101d72516",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "958a63b71c30de3f8bc3",
+    "url": "/static/js/2.b94c20fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b94c20fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce954e224e101d72516",
+    "url": "/static/js/main.cbd307c6.chunk.js"
+  },
+  {
+    "revision": "35701709370976000bf3",
+    "url": "/static/js/runtime-main.3bf825c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
